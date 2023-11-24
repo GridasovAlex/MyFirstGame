05575eb9673d0d114e36b411179368881a4d5966
Gridasov Aleksandr <gridasovalex19032003@gmail.com>
2023-11-24 16:04:43 +0300

    Add move

--- Alien_Inbasion_Game/alien_invasion.py
+++ Alien_Inbasion_Game/alien_invasion.py
@@ -22,12 +22,28 @@ class AlienInvasion():
         while True:
             self._check_events()
             self._update_screen()
+            self.ship.update_rect()
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                #ставим флаг перемещения
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                #убираем флаг перемещения
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
 
     def _update_screen(self):
             """Обновляет изображение на экране и отображает новый экран"""
--- Alien_Inbasion_Game/ship.py
+++ Alien_Inbasion_Game/ship.py
@@ -18,6 +18,16 @@ class Ship():
         #каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update_rect(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Рисуем корабль в текущей позиции"""
         self.screen.blit(self.image, self.rect)
